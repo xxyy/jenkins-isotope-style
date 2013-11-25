03c35503b38fedfd9ef9a8c2087625f23f7a22ab
xxyy <xxyy98@gmail.com>
2013-11-26 00:22:23 +0100

    Modify files to work with Windows, Change style

--- copy_styles.rb
+++ copy_styles.rb
@@ -1,11 +1,11 @@
 # Copy the styles from the master branch to the gh-pages branch
-`mkdir /tmp/jenkins-isotope-style`
-`cp *.css /tmp/jenkins-isotope-style`
+#`mkdir C:\\tmp\\jenkins-isotope-style`
+`copy jenkins-isotope-style.css C:\\tmp\\jenkins-isotope-style`
 `git checkout gh-pages`
 `git fetch origin`
 `git merge origin/gh-pages`
-`cp /tmp/jenkins-isotope-style/*.css ./`
-`git add *.css`
+`cp C:\\tmp\\jenkins-isotope-style\*.css .\\`
+`git add jenkins-isotope-style.css`
 `git commit -m"Updated styles"`
 `git push origin gh-pages`
 `git checkout master`
---
+++ sass-watch.bat
@@ -0,0 +1,4 @@
+@echo off
+echo WOW such watch
+sass --watch jenkins-isotope-style.scss:jenkins-isotope-style.css
+pause
